e92052c6d0b36d310d8998d955ab37b3898ba70f
Mustafa Kemal Aktaş <mustafaktaskemal@gmail.com>
2022-04-05 15:40:14 +0300

    Karakter eklendi. Hareket sistemine animasyon eklendi

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,13 +8,14 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody2D rb;
     Vector3 velocity;
+    public Animator animator;
 
-    float speedAmount = 7f;
-    float jumpAmount = 5f;
+    public float speedAmount = 9f;
+    public float jumpAmount = 7f;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
@@ -22,10 +23,16 @@ public class PlayerMovement : MonoBehaviour
     {
         velocity = new Vector3(Input.GetAxis("Horizontal"), 0f);
         transform.position += velocity * speedAmount * Time.deltaTime;
+        animator.SetFloat("Speed" , Mathf.Abs(Input.GetAxis("Horizontal")) );
 
         if (Input.GetKey(KeyCode.UpArrow) && Mathf.Approximately(rb.velocity.y, 0))
         {
             rb.AddForce(Vector3.up * jumpAmount, ForceMode2D.Impulse);
+            animator.SetBool("IsJumping", true);
+        }
+        if(animator.GetBool("IsJumping") && Mathf.Approximately(rb.velocity.y, 0))
+        {
+            animator.SetBool("IsJumping", false);
         }
 
         if (Input.GetAxisRaw("Horizontal") == -1 )
@@ -36,6 +43,6 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
- 
+
     }
 }
