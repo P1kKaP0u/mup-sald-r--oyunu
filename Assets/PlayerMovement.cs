d1b376a6c84909a775fbc6647ace03d607158b97
Mustafa Kemal Aktaş <mustafaktaskemal@gmail.com>
2022-04-06 15:21:57 +0300

    Saldırı Animasyonu eklendi ve butona atandı

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -21,11 +21,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Character Left-Right movement
         velocity = new Vector3(Input.GetAxis("Horizontal"), 0f);
         transform.position += velocity * speedAmount * Time.deltaTime;
         animator.SetFloat("Speed" , Mathf.Abs(Input.GetAxis("Horizontal")) );
 
-        if (Input.GetKey(KeyCode.UpArrow) && Mathf.Approximately(rb.velocity.y, 0))
+        //Character Jump
+        if (Input.GetKeyDown(KeyCode.UpArrow) && Mathf.Approximately(rb.velocity.y, 0))
         {
             rb.AddForce(Vector3.up * jumpAmount, ForceMode2D.Impulse);
             animator.SetBool("IsJumping", true);
@@ -35,6 +38,7 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("IsJumping", false);
         }
 
+        //Character Left-Right Rotation
         if (Input.GetAxisRaw("Horizontal") == -1 )
         {
             transform.rotation = Quaternion.Euler(0f, 180f, 0f);
@@ -42,6 +46,16 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetAxisRaw("Horizontal") == 1)
         {
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        //Character Attack
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("IsAttack" , true); 
+        }
+        else
+        {
+            animator.SetBool("IsAttack" , false);
         }
 
     }
